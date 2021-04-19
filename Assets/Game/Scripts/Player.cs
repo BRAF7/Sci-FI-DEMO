9131ff847fa3b0c5b41ef11536489e4a63c54ebd
Brian <brianbtfa@hotmail.com>
2021-04-19 17:58:15 -0500

    Shoot added

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,6 +7,11 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
     private float _speed = 3.5f;
     private float _gravity = 9.81f;
+    [SerializeField]
+    private GameObject shoot;
+    [SerializeField]
+    private GameObject _hitMarker_prefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +22,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            if(Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.name);
+                shoot.SetActive(true);
+            }
+        }
+        else
+        {
+            shoot.SetActive(false);
+        }
+
         CalculateMovement();
         if (Input.GetKey(KeyCode.Escape))
             Cursor.lockState = CursorLockMode.None;
     }
+
     void CalculateMovement()
     {
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
